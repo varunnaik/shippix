29baa4c7b1f827c46c6d8e575b75c5fca3dfa98b
Varun Naik <varun.naik@gmail.com>
2017-10-22 20:36:46 +1100

    Save MMSI and ship info

--- ais_capture.py
+++ ais_capture.py
@@ -15,7 +15,7 @@ import sys
 import sqlite3
 import datetime
 import requests
-from itushipinfo import identify_ship
+from itushipinfo import itu_identify_vessel
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 #logging.basicConfig(filename='ais-server.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -33,104 +33,112 @@ logging.debug("successful server intiation")
 mmsilookups_inflight = []
 
 def logais(ais):
-	c.execute("INSERT INTO ais VALUES (?, ?)", (datetime.datetime.utcnow().isoformat(), json.dumps(ais)))
-	conn.commit()
+    c.execute("INSERT INTO ais VALUES (?, ?)", (datetime.datetime.utcnow().isoformat(), json.dumps(ais)))
+    conn.commit()
 
 def logtraffic(ais):
-	c.execute('INSERT INTO trafficlog VALUES (?, ?)', (datetime.datetime.utcnow().isoformat(), ais["mmsi"]))
-	conn.commit()
+    c.execute('INSERT INTO trafficlog VALUES (?, ?)', (datetime.datetime.utcnow().isoformat(), ais["mmsi"]))
+    conn.commit()
 
+def updatevessel(mmsi, ignored, identified, fullinfo):
+    if fullinfo == None:
+        c.execute('INSERT INTO vesselinfo (mmsi, ignored, identified) VALUES (?, ?, ?)', (mmsi, ignored, identified))
+    else:
+        c.execute('INSERT INTO vesselinfo VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (mmsi, ignored, fullinfo['url'], identified, fullinfo['name'], fullinfo['details'], fullinfo['size'], fullinfo['gross_tonnage'], fullinfo['notes'], fullinfo['flag']))
+    conn.commit()
 
 def shouldprocess(ais):
-	c.execute("SELECT * FROM mmsi WHERE mmsi = ?", (ais['mmsi'],))
-	try:
-		mmsi, ignored = c.fetchone()
-	except TypeError:
-		return True
+    '''Return tuple (identified, ignored)'''
+    c.execute("SELECT identified, ignored FROM vesselinfo WHERE mmsi = ?", (ais['mmsi'],))
+    status = c.fetchone()
+    if status == None:
+        return (False, False)
+    return status
 
-	return ignored == 0
-
-def get_info(mmsi):
-	r = requests.get('http://www.vesselfinder.com/vessels/shipsearch?term=%s'% (mmsi,))
-	print r.text
-
-def lookupmmsi(mmsi):
-	# Perform lookup then: 
-
-	c.execute("INSERT INTO mmsi VALUES (?,0)", (ais["mmsi"],))
-	conn.commit()
-
-
-
-
-get_vessel_details(246028000)
+def identifyvessel(ais):
+    shipinfo = itu_identify_vessel(ais)
+    if shipinfo:
+        updatevessel(ais['mmsi'], ignored=False, identified=True, fullinfo=shipinfo)
+    else:
+        updatevessel(ais['mmsi'], ignored=True, identified=False, fullinfo=None)
 
 while True:
 
+    try:
+        data, addr = serverSock.recvfrom(1024)
+        payload=data.split(",")
 
-    data, addr = serverSock.recvfrom(1024)
-    payload=data.split(",")
+        messagecontainer = ""
 
-    messagecontainer = ""
+        pad = int(payload[-1].split('*')[0][-1])
+        msglength = int(payload[1])
+        msgpart = int(payload[2])
 
-    pad = int(payload[-1].split('*')[0][-1])
-    msglength = int(payload[1])
-    msgpart = int(payload[2])
+        if msglength == 1:
+            rawmessage = payload[5]
+            decodedmessage = ais.decode(rawmessage,pad)
 
-    if msglength == 1:
-	    rawmessage = payload[5]
-	    decodedmessage = ais.decode(rawmessage,pad)
+            # logging.info("SUCCESS: decoded message -> %s", str(decodedmessage))
+            # print json.dumps(decodedmessage)
+            
+            logtraffic(decodedmessage)
+            identified, ignored = shouldprocess(decodedmessage)
+            logging.info(str(decodedmessage['mmsi']) + " ignored = " + str(ignored))
+            if not identified and not ignored:
+                identifyvessel(decodedmessage)
+            elif not ignored:
+                # Process image
+                pass
 
-	    logging.info("SUCCESS: decoded message -> %s", str(decodedmessage))
-	    print json.dumps(decodedmessage)
-	    print "--------------"
-	    logtraffic(decodedmessage)
-	    if shouldprocess(decodedmessage):
-	    	logais(decodedmessage)
-	    	ship = identify_ship(decodedmessage)
+            messagecontainer = ""
+        
+        else: 
+            msgcomplete = 0
+            messagecontainer += payload[5]
+            
+            while (msgcomplete == 0): 
+                data, addr = serverSock.recvfrom(1024)
+                payload=data.split(",")
+                rawmessage = payload[5]
+                msglength = int(payload[1])
+                msgpart = int(payload[2])
 
-	    messagecontainer = ""
-    
-    else: 
-    	msgcomplete = 0
-    	messagecontainer += payload[5]
-    	
-    	while (msgcomplete == 0): 
-			data, addr = serverSock.recvfrom(1024)
-			payload=data.split(",")
-			rawmessage = payload[5]
-			msglength = int(payload[1])
-			msgpart = int(payload[2])
+                messagecontainer += rawmessage
 
-			messagecontainer += rawmessage
+                logging.debug("incoming data -> %s", str(data))
+                logging.debug("message part -> %s", str(msgpart))
+                logging.debug("message length -> %s", str(msglength))
+                logging.debug("pad ->  %s", str(pad))
+                logging.debug("raw message -> %s", str(rawmessage))
+                logging.debug("messagecontainer -> %s", str(messagecontainer))
 
-			logging.debug("incoming data -> %s", str(data))
-			logging.debug("message part -> %s", str(msgpart))
-			logging.debug("message length -> %s", str(msglength))
-			logging.debug("pad ->  %s", str(pad))
-			logging.debug("raw message -> %s", str(rawmessage))
-			logging.debug("messagecontainer -> %s", str(messagecontainer))
+                
+                if (msglength == msgpart):
+                    pad = int(payload[-1].split('*')[0][-1])
+                    
+                    #remove escape from test udp
+                    messagecontainer = messagecontainer.replace("\\","")
 
-			
-			if (msglength == msgpart):
-				pad = int(payload[-1].split('*')[0][-1])
-				
-				#remove escape from test udp
-				messagecontainer = messagecontainer.replace("\\","")
+                    logging.debug("final pad ->  %s", str(pad))
+                    logging.debug("final messagecontainer -> %s", str(messagecontainer))
 
-				logging.debug("final pad ->  %s", str(pad))
-				logging.debug("final messagecontainer -> %s", str(messagecontainer))
+                    decodedmessage = ais.decode(messagecontainer,pad)
+                    
+                    # logging.info("SUCCESS: decoded multipart message -> %s", str(decodedmessage))
+                    # print json.dumps(decodedmessage)
+                    logtraffic(decodedmessage)
+                    identified, ignored = shouldprocess(decodedmessage)
+                    if not identified and not ignored:
+                        identifyvessel(decodedmessage)
+                    elif not ignored:
+                        # Process image
+                        pass
 
-				decodedmessage = ais.decode(messagecontainer,pad)
-				
-				logging.info("SUCCESS: decoded multipart message -> %s", str(decodedmessage))
-				print json.dumps(decodedmessage)
-				logtraffic(decodedmessage)
-				if shouldprocess(decodedmessage):
-					logais(decodedmessage)
+                    messagecontainer = ""
+                    msgcomplete = 1
+    except:
+        logging.error(sys.exc_info())
 
-				messagecontainer = ""
-				msgcomplete = 1
 
 
 # Note code above implemented a strip of "\" from incoming messages given need to add escape when socat-ing on terminal; should be removed in prod
