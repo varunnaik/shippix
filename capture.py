2d38f538ef69a12eb143d60cd89066a9bf647320
Varun Naik <varun.naik@gmail.com>
2017-11-05 17:38:11 +1100

    Better timer handling

--- capture.py
+++ capture.py
@@ -11,27 +11,24 @@ class Capture:
         self.activecaptures = {}
         self.captureimages = {}
 
+
     def start(self, code, captureSeconds=60):
         '''Given an arbitrary code, captures images with that codename till told to stop'''
         if code in self.activecaptures:
         	return False
         print "Start capture"
-        self.activecaptures[code] = { 'capture': True, 'end': datetime.datetime.now() + datetime.timedelta(seconds = captureSeconds), 'seq': 0 }
         self.captureimages[code] = []
-        # Note: All captures will have the same code...
-        if not self.timer:
-            self.timer = Timer(self.capture_image, 1, [code])
-            self.timer.start()
+        self.activecaptures[code] = { 'capture': True, 'end': datetime.datetime.now() + datetime.timedelta(seconds = captureSeconds), 'seq': 0, 'timer': None }
+        self.activecaptures[code]['timer'] = Timer(self.capture_image, 1, [code])
+        self.timer.start()
         self.capture_image(code)
 
     def capture_image(self, code):
     	'''Capture an image provided the capture has not been stopped'''
         if not self.activecaptures[code]['capture'] \
                 or self.activecaptures[code]['end'] <= datetime.datetime.now(): # If this capture is finished
+            self.activecaptures[code]['timer'].cancel()    
             del self.activecaptures[code] # Then delete the capture
-            if len(self.activecaptures) == 0: # If this was the last active capture
-                self.timer.cancel()    # Then also delete the timer itself
-                self.timer = None
         else:
             self.activecaptures[code]['seq'] += 1;
             filename = "img/%s_%s.jpg" % (code, self.activecaptures[code]['seq'])
